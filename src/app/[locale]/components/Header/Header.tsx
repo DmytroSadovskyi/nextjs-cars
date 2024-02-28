79730c0866a3238c735b79871a97bbbd91485ae1
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2024-02-28 23:26:55 +0200

    add animation for mobile menu

--- src/app/[locale]/components/Header/Header.tsx
+++ src/app/[locale]/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Squash as Hamburger } from "hamburger-react";
+import { motion, AnimatePresence } from "framer-motion";
 import { useTranslations } from "next-intl";
 import { useState } from "react";
 import Image from "next/image";
@@ -24,47 +25,56 @@ export default function Header() {
   };
 
   return (
-    <header className={styles.header}>
-      <Link href="/">
-        <Image
-          priority={true}
-          className={styles.logo}
-          src={logo}
-          alt="logo"
-          width={30}
-          height={30}
+    <>
+      <header className={styles.header}>
+        <Link href="/">
+          <Image
+            priority={true}
+            className={styles.logo}
+            src={logo}
+            alt="logo"
+            width={30}
+            height={30}
+          />
+        </Link>
+
+        <Hamburger
+          toggled={isMenuOpen}
+          toggle={toggleMenu}
+          size={28}
+          hideOutline={false}
+          label="menu-icon"
         />
-      </Link>
 
-      <Hamburger
-        toggled={isMenuOpen}
-        toggle={toggleMenu}
-        size={28}
-        hideOutline={false}
-        label="menu-icon"
-      />
-
-      <nav className={styles.nav}>
-        <NavigationLink href="/">{t("home")}</NavigationLink>
-        <NavigationLink href="/catalog">{t("catalog")}</NavigationLink>
-        <NavigationLink href="/favorites">{t("favorites")}</NavigationLink>
-        <LocaleSwitcher />
-      </nav>
-
-      {isMenuOpen && (
-        <div className={styles.mobileMenu}>
-          <NavigationLink href="/" onClick={closeMenu}>
-            {t("home")}
-          </NavigationLink>
-          <NavigationLink href="/catalog" onClick={closeMenu}>
-            {t("catalog")}
-          </NavigationLink>
-          <NavigationLink href="/favorites" onClick={closeMenu}>
-            {t("favorites")}
-          </NavigationLink>
+        <nav className={styles.nav}>
+          <NavigationLink href="/">{t("home")}</NavigationLink>
+          <NavigationLink href="/catalog">{t("catalog")}</NavigationLink>
+          <NavigationLink href="/favorites">{t("favorites")}</NavigationLink>
           <LocaleSwitcher />
-        </div>
-      )}
-    </header>
+        </nav>
+      </header>
+      <AnimatePresence initial={false} onExitComplete={() => null}>
+        {isMenuOpen && (
+          <motion.div
+            className={styles.mobileMenu}
+            initial={{ opacity: 0, y: "-100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            exit={{ opacity: 0, y: "-100%" }}
+          >
+            <NavigationLink href="/" onClick={closeMenu}>
+              {t("home")}
+            </NavigationLink>
+            <NavigationLink href="/catalog" onClick={closeMenu}>
+              {t("catalog")}
+            </NavigationLink>
+            <NavigationLink href="/favorites" onClick={closeMenu}>
+              {t("favorites")}
+            </NavigationLink>
+            <LocaleSwitcher />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
